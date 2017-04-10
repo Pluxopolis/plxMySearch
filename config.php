e6e04e77c8058ab1a7282ee40ec6454b302be105
Stephane <Stphane@pluxml.org>
2017-04-10 15:09:51 +0200

    controle existence PLX_MYMULTILINGUE + maj infos

--- config.php
+++ config.php
@@ -9,9 +9,11 @@ $aLangs = array($plxAdmin->aConf['default_lang']);
 
 # Si le plugin plxMyMultiLingue est installé on filtre sur les langues utilisées
 # On garde par défaut le fr si aucune langue sélectionnée dans plxMyMultiLingue
-$langs = plxMyMultiLingue::_Langs();
-$multiLangs = empty($langs) ? array() : explode(',', $langs);
-$aLangs = $multiLangs;
+if(defined('PLX_MYMULTILINGUE')) {
+	$langs = plxMyMultiLingue::_Langs();
+	$multiLangs = empty($langs) ? array() : explode(',', $langs);
+	$aLangs = $multiLangs;
+}
 
 if(!empty($_POST)) {
 	$plxPlugin->setParam('frmDisplay', $_POST['frmDisplay'], 'numeric');
